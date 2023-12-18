7107dc45943788afae3a14894e9c79426e6c8d16
hassaammgl <h455a4mmehtab@gmail.com>
2023-12-18 10:52:27 +0500

    hassaam is here

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,9 +5,7 @@ export default function Home() {
   return (
    <MaxWidthWrapper className=''>
      <div className=" py-20 mx-auto text-center  flex flex-col items-center max-w-3xl">
-        <h2>Hello World</h2>
-        
-        {/* All the home page section here */}
+        Hassaam is testing
      </div>
    </MaxWidthWrapper>
   )
